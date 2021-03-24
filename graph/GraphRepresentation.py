ac34aea307fa696f6d579cceb9a972d50e676fc4
abegpatel <65013313+abegpatel@users.noreply.github.com>
2021-03-24 23:42:59 +0530

    Add files via upload

---
+++ graph/GraphRepresentation.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 20:37:45 2021
+
+@author: Abeg
+"""
+#Graph Representation
+#edge list
+graph=[[0,2],[2,3],[2,1],[1,3]]
+
+#adjacent list
+graph=[[2],[2,3],[0,1,3],[1,2]]
+
+#adjacent matrix
+graph=[
+  [0,0,1,0],
+  [0,0,1,1],
+  [1,1,0,1],
+  [0,1,1,0]
+]
+
+graph={
+  0:[0,0,1,0],
+  1:[0,0,1,1],
+  2:[1,1,0,1],
+  3:[0,1,1,0]}
